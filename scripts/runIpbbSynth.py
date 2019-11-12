4e5190f4b7a41689db9a5c1fb6b6bac83f351ab1
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2019-11-12 14:34:09 +0100

    updated ipbb directory name

--- scripts/runIpbbSynth.py
+++ scripts/runIpbbSynth.py
@@ -134,7 +134,10 @@ def main():
     # Create MP7 tag name for ugt    
     mp7fw_ugt = args.mp7tag + mp7fw_ugt_suffix
     
-    ipbb_dir = os.path.join(args.path, project_type, args.mp7tag, args.menuname, args.build)
+    #ipbb_dir = os.path.join(args.path, project_type, args.mp7tag, args.menuname, args.build)
+    # HB 2019-11-12: inserted mp7_ugt tag and vivado version in directory name
+    vivado_version = "vivado_" + args.vivado
+    ipbb_dir = os.path.join(args.path, project_type, args.ugt, vivado_version, args.mp7tag, args.menuname, args.build)
 
     if os.path.isdir(ipbb_dir):
         raise RuntimeError("build area alredy exists: {}".format(ipbb_dir))
