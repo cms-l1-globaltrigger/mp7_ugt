5ef9408f9509ac856fa014638df32319e942e22e
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2020-03-04 13:31:59 +0100

    fixed typo

--- firmware/hdl/data/gtl/sum_mass_calc.vhd
+++ firmware/hdl/data/gtl/sum_mass_calc.vhd
@@ -2,6 +2,7 @@
 -- Calculation of sum mass of 3 objects.
 
 -- Version history:
+-- HB 2020-03-04: Added attribute use_dsp.
 -- HB 2020-02-18: First design.
 
 library ieee;
@@ -24,7 +25,7 @@ end sum_mass_calc;
 
 architecture rtl of sum_mass_calc is
 
-    signal sum_mass_1 : std_logic_vector(DATA_WIDTH downto 0);
+    signal sum_mass_1_2 : std_logic_vector(DATA_WIDTH downto 0);
     
     attribute use_dsp : string;
     attribute use_dsp of sum_mass_1_2 : signal is "yes";
