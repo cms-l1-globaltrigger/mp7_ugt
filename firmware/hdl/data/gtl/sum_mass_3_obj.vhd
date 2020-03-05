984b9101e465a59da8a80f37a22eef11832ff9cf
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2020-03-05 16:12:03 +0100

    bug fixed

--- firmware/hdl/data/gtl/sum_mass_3_obj.vhd
+++ firmware/hdl/data/gtl/sum_mass_3_obj.vhd
@@ -24,7 +24,7 @@ architecture rtl of sum_mass_3_obj is
 
     type data_vec_array is array(0 to N_OBJ-1, 0 to N_OBJ-1) of std_logic_vector(DATA_WIDTH-1 downto 0);
     signal data_vec : data_vec_array;
-    type sum_temp_array is array(0 to N_OBJ-1, 0 to N_OBJ-1, 0 to N_OBJ-1) of std_logic_vector(DATA_WIDTH-1 downto 0);
+    type sum_temp_array is array(0 to N_OBJ-1, 0 to N_OBJ-1, 0 to N_OBJ-1) of std_logic_vector(DATA_WIDTH+1 downto 0);
     signal sum_temp : sum_temp_array;
     
 begin
@@ -42,10 +42,10 @@ begin
             l3_sum: for k in 0 to N_OBJ-1 generate
                 sum_i: if j>i and k>i and k>j generate
                     sum_mass_calc_i: entity work.sum_mass_calc
-                        generic map(DATA_WIDTH)  
+                        generic map(DATA_WIDTH+1)  
                         port map(data_vec(i,j), data_vec(i,k), data_vec(j,k), sum_temp(i,j,k));
                 end generate sum_i;
-                l4_sum: for l in 0 to DATA_WIDTH-1 generate
+                l4_sum: for l in 0 to DATA_WIDTH+1 generate
                     sum_o(i,j,k,l) <= sum_temp(i,j,k)(l);
                 end generate l4_sum;    
             end generate l3_sum;    
